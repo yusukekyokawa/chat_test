e80a3de16053c65a24cc0e17418cf497f2afc82e
yusukekyokawa <medakasuisui598@gmail.com>
2019-03-07 18:50:22 +0900

    socketiowo

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express')
 var app = express();
 var http = require('http').Server(app)
+const io = require('socket.io')(http)
 const PORT = process.env.PORT || 7000;
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+})
+io.on('connection', function (socket) {
+  console.log('connected')
 })
 
 http.listen(PORT, function () {
