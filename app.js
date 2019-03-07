73780cf022a492c2fc17e25760788456186d9ae7
yusukekyokawa <medakasuisui598@gmail.com>
2019-03-07 18:55:38 +0900

    一方向の通信ができた

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 io.on('connection', function (socket) {
-  console.log('connected')
+  socket.on('message', function (msg) {
+    console.log('message: ' + msg)
+  })
 })
 
 http.listen(PORT, function () {
